f654cda59b3ea2d26f513efa4d12e0965a9ca4c7
mnkhod <mnkhod.dev@gmail.com>
2023-06-19 00:21:33 +0800

    Added Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,33 @@
 import { Icon } from "@iconify/react";
 import Image from "next/image";
 import { Button } from "@rewind-ui/core";
+import Link from "next/link";
 
 export default function Home() {
-  const navIconStyle = "w-auto h-4 md:h-5";
+  const navIconStyle = "w-auto h-8";
 
   return (
     <main className="w-full flex justify-center">
       <div className="md:w-9/12 flex flex-col items-center">
-
         <div className="flex justify-center py-16 border border-2 border-l-0 border-r-0 w-full">
           <div className="w-9/12 flex flex-col gap-2 md:gap-0 md:flex-row items-center justify-between">
             <h1 className="text-2xl md:text-3xl">{"Amu's Art Gallery"}</h1>
-            <ul className="flex gap-2 md:gap-6 items-center">
+            <ul className="flex gap-4 md:gap-6 items-center">
               <li>
-                <p className="text-sm md:text-lg">Buy Now</p>
+                <a href="">
+                  <p className="text-sm md:text-lg">Buy Now</p>
+                </a>
               </li>
               <li className="h-full w-0.5 bg-gray-300"></li>
               <li>
-                <Icon icon="devicon:facebook" className={navIconStyle} />
+                <a href="https://www.facebook.com/gunigtai.amu">
+                  <Icon icon="devicon:facebook" className={navIconStyle} />
+                </a>
               </li>
               <li>
-                <Icon icon="skill-icons:instagram" className={navIconStyle} />
+                <a href="https://www.instagram.com/namuun_afk">
+                  <Icon icon="skill-icons:instagram" className={navIconStyle} />
+                </a>
               </li>
             </ul>
           </div>
@@ -35,20 +41,22 @@ export default function Home() {
               Amu,Master Drawings
             </h1>
 
-            <Button
-              animation="pulse"
-              className="w-11/12 md:w-6/12"
-              color="white"
-              tone="transparent"
-              shadow="sm"
-              radius="full"
-              size="lg"
-              withRing={false}
-            >
-              <p className="font-light text-xs tracking-[5px]">
-                Explore her world now
-              </p>
-            </Button>
+            <Link href="https://www.instagram.com/namuun_afk" className="w-full flex justify-center">
+              <Button
+                animation="pulse"
+                className="w-11/12 md:w-6/12"
+                color="white"
+                tone="transparent"
+                shadow="sm"
+                radius="full"
+                size="lg"
+                withRing={false}
+              >
+                <p className="font-light text-xs tracking-[5px]">
+                  Explore her world now
+                </p>
+              </Button>
+            </Link>
           </div>
           <div className="relative h-[400px] w-full">
             <Image src={"/0.jpg"} alt={"Art"} fill />
