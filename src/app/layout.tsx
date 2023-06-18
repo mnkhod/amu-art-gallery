140f0624ec92edef42e37e4fde80fea975aee6a3
mnkhod <mnkhod.dev@gmail.com>
2023-06-19 00:07:44 +0800

    Initial Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Marcellus } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Marcellus({ weight: "400", subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
